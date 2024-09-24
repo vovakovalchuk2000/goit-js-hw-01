635f39e80295b28a2fd8154c4a54b28584972cf1
Vova <bobavova12@gmail.com>
2024-09-24 12:42:19 +0300

    fix

--- js/task-2.js
+++ js/task-2.js
@@ -1,5 +1,5 @@
 function getShippingMessage(country, price, deliveryFee) {
-  const totalPrice = price * deliveryFee;
+  const totalPrice = price + deliveryFee;
   return `Shipping to ${country} will cost ${totalPrice} credits`;
 }
 console.log(getShippingMessage('Australia', 120, 50)); // "Shipping to Australia will cost 170 credits"
